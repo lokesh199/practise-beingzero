d57130cddbe3d801eefdf27a5854f78924cf6dc6
lokesh199 <lokesh.agarwal.1999@gmail.com>
2020-01-08 14:45:32 +0530

    incomplete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ColorComponent } from './color/color.component';
+import { CountryComponent } from './country/country.component';
+import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'colors', component: ColorComponent },
+  { path: 'country', component: CountryComponent },
+  { path: 'nav', component: NavComponent },
+  {path: 'user', component: UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
